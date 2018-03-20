54039540ffe81d0a16543396d24a787df07992a1
王锐 <wangrui@bangbangbang.cn>
2018-03-20 18:45:38 +0800

    no message

---
+++ StarShareCorePodSpec/StarShareCorePodSpec/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  StarShareCorePodSpec
+//
+//  Created by BUBUKO on 2018/3/20.
+//  Copyright © 2018年 bugu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    /*
+     StarShareCore.default.request(V2EX(), to: Node.self).startWithResult { r in
+     print(r.value ?? "null")
+     
+     let cacheValue = try! CacheCore.responseCache.fetch(ofType: Node.self, forDomainBean: V2EX())
+     print(cacheValue ?? "null")
+     }
+     */
+    StarShareCore.default.loadCacheIfNeed(V2EX(), to: Node.self).startWithResult { r in
+      print(r.value ?? "null")
+    }
+  }
+}
+
